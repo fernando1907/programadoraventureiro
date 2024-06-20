8284f5380e68db2fa6529dd7a4ba39d6087af2e7
programadoraventureiro <lfrocha@cableware.com.br>
2024-06-20 17:56:32 -0300

    Aula 14 page 1

--- aula.py
+++ aula.py
@@ -18,8 +18,27 @@ import flet as ft
 
 class App:
     def __init__(self, page: ft.Page):
-        mensagem = ft.Text(value='Olá mundo')
-        page.add(mensagem)
+        #
+        # formas de atribuir cores
+        #
+        #page.bgcolor = 'Green'
+        #page.bgcolor = '#B12B12'
+        page.bgcolor = ft.colors.AMBER        
+        page.horizontal_alignment = ft.CrossAxisAlignment.STRETCH
+        page.vertical_alignment = ft.MainAxisAlignment.CENTER
+        #page.padding = ft.padding.all(10)
+        #page.padding = ft.padding.symmetric(vertical=10, horizontal=100) 
+        page.padding = ft.padding.only(top=10, left=20, right=20, bottom=200)
+        page.spacing = 100
+        # titulo na barra do celular no topo da tela
+        page.title = 'Flet - aula'
+        page.add(
+            ft.Text(value='Ola mundo'),
+            ft.Container(ft.Text(value='Ola mundo 2'), bgcolor='RED'),
+            ft.Container(ft.Text(value='Ola mundo 2'), bgcolor='RED')
+        )     
+        page.update()
 
+#
 
 ft.app(target=App)
