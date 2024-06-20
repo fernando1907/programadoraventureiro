7c29a63072fda4bcd26cd1208765651318d98898
programadoraventureiro <lfrocha@cableware.com.br>
2024-06-20 15:56:52 -0300

    Aula 11

---
+++ aula.py
@@ -0,0 +1,23 @@
+import flet as ft
+
+
+#
+#  Aula 11
+#
+# definição da caracteristicas e funções da pagina e sistema operacional
+#
+#def main(page: ft.Page):
+#    pass
+
+
+# chama a função definida na main
+#ft.app(target=main)
+
+# chmando na forma de classes
+
+class App:
+    def __init__(self, page:ft.Page):
+        pass
+
+
+ft.app(target=App)
