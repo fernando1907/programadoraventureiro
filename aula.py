b64152f1cdeee337bb2c1048408b85e0fb4b2ded
programadoraventureiro <lfrocha@cableware.com.br>
2024-06-21 09:51:48 -0300

    aula15

--- aula.py
+++ aula.py
@@ -32,6 +32,30 @@ class App:
         page.spacing = 100
         # titulo na barra do celular no topo da tela
         page.title = 'Flet - aula'
+        # Propriedade  do aplicativo - janela
+        #
+        # PEMITE QUE A PAGINA FIQUE NA FRENTE DE TODAS
+        page.window_always_on_top = True
+        page.window_title_bar_hidden = False
+        page.window_frameless = False
+        page.window_full_screen = False
+        page.window_height = 300
+        page.window_max_height = 900
+        page.windows_min_height = 200
+        page.windows_widht = 600
+        page.window_max_width = 900
+        page.window_min_width = 200
+        page.window.resizable = True
+        page.window_top = 100
+        page.window_left = 900
+        page.window_movable = True
+        page.window_prevent_close = True
+        page.window_progress_bar = 0.5
+        
+        
+        
+        
+        
         page.add(
             ft.Text(value='Ola mundo'),
             ft.Container(ft.Text(value='Ola mundo 2'), bgcolor='RED'),
